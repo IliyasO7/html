b9096a67068a81378b37ff000135c081fb3b64d5
7083375809 <shaikhiliyas343@gmail.com>
2022-10-10 14:30:36 +0530

    merge all files

--- index.js
+++ index.js
@@ -36,6 +36,42 @@ function onSubmit(e){
 
 }
 
+*/
+/*
+const myform = document.querySelector('#my-form');
+const nameInput = document.querySelector('#fName');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+
+myform.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    if(nameInput.value ==='' || emailInput.value ==='')
+    {
+        alert('please enter field');
+        return;
+
+    }
+
+   let myObj = {
+      name : nameInput.value,
+      emailid : emailInput.value
+    };
+
+    let myObj_serialized = JSON.stringify(myObj);
+
+    localStorage.setItem('myObj',myObj_serialized);
+    
+}
+*/
+
+
+
+
 
 /*
 const btn = document.querySelector('.btn');
@@ -45,4 +81,81 @@ btn.addEventListener('click', (e) => {
     document.querySelector('#my-form').style.backgroundColor = '#ccc';
 });
 */
+
+
+const myform = document.querySelector('#my-form');
+const nameInput = document.querySelector('#fName');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+
+
+myform.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+   if(nameInput.value ==='' || emailInput.value ==='')
+    {
+        alert('please enter field');
+        return;
+
+    }
+   
+
+    
+   let myObj = {
+      name: nameInput.value,
+      email: emailInput.value
+    };
+
+    //let myObj_serialized = JSON.stringify(myObj);
+   
+    localStorage.setItem(myObj.email,JSON.stringify(myObj));
+    showNoOfUsers(myObj);
+
+    
+    
+
+
+
+    
+
+
+   
+}
+function showNoOfUsers(myObj)
+    {
+        
+        //const keyArr = Object.keys(myObj);
+        showNewUSerOnScreen(myObj)
+        
+/*
+        keyArr.forEach(key =>
+            {
+                console.log(myObj[key]);
+            });*/
+
+        
+        
+    }
+
+    function showNewUSerOnScreen(myObj){
+        
+            console.log(localStorage.getItem(myObj.email));
+        
+
+    } 
+   /* function deleteUser(emailInput){
+        console.log(emailInput);
+        localStorage.removeItem(emailInput);
+        
+    }
+    */
+
+
+   
+
+
 
